f712706563463cd953789a5f32d6f080bb6d1fdb
Christian Autermann <c.autermann@52north.org>
2014-11-11 09:03:47 +0100

    attach input handler in .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,14 @@
 
-.onAttach <- function(libname, pkgname) {
-    shiny::registerInputHandler("n52.datetime", function(x, shinysession, name) {
-        if (is.null(x)) return(NA);
-        if (is.numeric(x)) return(as.POSIXct(x, origin="1970-01-01", tz="UTC"));
-        return(strptime(x, "%Y-%m-%dT%H:%M:%OS", tz = "UTC"));
-    }, force=TRUE);
+.timeInputHandler <- function(x, shinysession, name) {
+    if (is.null(x)) return(NA);
+    if (is.numeric(x)) return(as.POSIXct(x, origin="1970-01-01", tz="UTC"));
+    return(strptime(x, "%Y-%m-%dT%H:%M:%OS", tz = "UTC"));
+}
+
+.onLoad <- function(libname, pkgname) {
+    shiny::registerInputHandler("n52.datetime", .timeInputHandler);
+}
+
+.onUnload <- function(libpath) {
+    shiny::removeInputHandler("n52.datetime")
 }
