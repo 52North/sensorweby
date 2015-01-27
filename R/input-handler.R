abb6b5437af8c070710effb2ddc719cd74ea6291
Christian Autermann <c.autermann@52north.org>
2015-01-27 09:57:10 +0100

    is.na(NA) is not a good if condition

--- R/input-handler.R
+++ R/input-handler.R
@@ -1,7 +1,7 @@
 
 
 .timeInputHandler <- function(x, shinysession, name) {
-    if (is.null(x) || is.na(NA))
+    if (is.null(x) || is.na(x))
         as.POSIXct(NA)
     else if (is.numeric(x))
         as.POSIXct(x, origin="1970-01-01", tz = "UTC")
